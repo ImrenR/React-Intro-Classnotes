09132973b361099846e0abb6e652822dfedcf7c9
Imren <imrenrahbay@gmail.com>
2024-12-29 17:58:48 +0100

    import

--- sass-styling/src/App.js
+++ sass-styling/src/App.js
@@ -1,11 +1,13 @@
+import Header from "./components/Header";
 import data from "./data"
 import "./scss/app.scss"
 function App() {
    console.log(data);
   return (
-    <div className="App">
-      <h1>React Lesson 3</h1>
-    </div>
+    <>
+     
+      <Header/>
+    </>
   );
 }
 
