821c79a2133f032eaf38154e62ce15c9c90da498
Imren <imrenrahbay@gmail.com>
2024-12-29 17:38:24 +0100

    sass

---
+++ sass-styling/src/App.js
@@ -0,0 +1,12 @@
+import data from "./data"
+import "./scss/app.scss"
+function App() {
+   console.log(data);
+  return (
+    <div className="App">
+      <h1>React Lesson 3</h1>
+    </div>
+  );
+}
+
+export default App;
